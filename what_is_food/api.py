973bf0359829ed90c72006524eb89f94be3fe68d
ashish-greycube <mr.ashish.shah@gmail.com>
2020-06-21 04:56:47 +0000

    fix:landing page changed

--- what_is_food/api.py
+++ what_is_food/api.py
@@ -3,4 +3,4 @@ from frappe.sessions import Session
 import frappe
 
 def on_session_creation(login_manager):
-	frappe.local.response["home_page"] = "/desk#Form/Track%20Me"
+	frappe.local.response["home_page"] = "/desk#trackme"
