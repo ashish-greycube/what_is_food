e0a0fbdce7188a6da48cbeca8688f989e9357e53
ashish-greycube <mr.ashish.shah@gmail.com>
2020-07-03 21:52:42 +0530

    js

--- what_is_food/what_is_food/doctype/geo_location_test/geo_location_test.js
+++ what_is_food/what_is_food/doctype/geo_location_test/geo_location_test.js
@@ -1,8 +1,32 @@
-// Copyright (c) 2020, GreyCube Technologies and contributors
-// For license information, please see license.txt
-
 frappe.ui.form.on('Geo Location Test', {
-	// refresh: function(frm) {
+	am_in_office: function (frm) {
 
-	// }
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(showPosition);
+		} else {
+			alert("Geolocation is not supported by this browser.");
+		}
+
+		function showPosition(position) {
+			alert('You are ' + calcCrow(position.coords.latitude, position.coords.longitude, frm.doc.latitude, frm.doc.longitude).toFixed(1) + ' kms away from office');
+		}
+		//This function takes in latitude and longitude of two location and returns the distance between them as the crow flies (in km)
+		function calcCrow(lat1, lon1, lat2, lon2) {
+			var R = 6371; // km
+			var dLat = toRad(lat2 - lat1);
+			var dLon = toRad(lon2 - lon1);
+			var lat1 = toRad(lat1);
+			var lat2 = toRad(lat2);
+
+			var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+				Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+			var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+			var d = R * c;
+			return d;
+		}
+		// Converts numeric degrees to radians
+		function toRad(Value) {
+			return Value * Math.PI / 180;
+		}
+	}
 });
